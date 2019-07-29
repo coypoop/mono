e221f68c80e6b2da84ef77556c32236500bb5a65
Maya Rashish <coypu@sdf.org>
2019-07-29 22:44:24 +0300

    [XXX do not upstream] Workaround apparent mono bug in sem_destroy
    we're destroying semaphores that other threads are waiting on.
    netbsd returns EBUSY in this case.

--- mono/utils/mono-os-semaphore.h
+++ mono/utils/mono-os-semaphore.h
@@ -190,6 +190,8 @@ mono_os_sem_destroy (MonoSemType *sem)
 	int res;
 
 	res = sem_destroy (sem);
+	if ((res != 0) && (errno = EBUSY))
+		res = 0;
 	if (G_UNLIKELY (res != 0))
 		g_error ("%s: sem_destroy failed with \"%s\" (%d)", __func__, g_strerror (errno), errno);
 }
